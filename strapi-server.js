3bbef788943c1abbdc98c0ed64d666e0fdd63b62
mjnoach <a.siennkiewicz@gmail.com>
2022-09-22 22:34:10 +0200

    install node-schedule and set up a test job

--- strapi-server.js
+++ strapi-server.js
@@ -1,3 +1,18 @@
-'use strict';
+"use strict";
 
-module.exports = require('./dist/server');
+const server = require("./dist/server");
+const schedule = require("node-schedule");
+
+module.exports = {
+  ...server.default,
+  async bootstrap({ strapi }) {
+    const service = await strapi
+      .plugin("cronjob-manager")
+      .service("myService")
+      .getWelcomeMessage();
+    console.log(service);
+    const job = schedule.scheduleJob("* * * * * *", function () {
+      console.log("🚀");
+    });
+  },
+};
