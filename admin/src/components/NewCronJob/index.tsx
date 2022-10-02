9e915f34af1ca4f4bc8ead106840e650b599ed40
mjnoach <a.siennkiewicz@gmail.com>
2022-10-02 11:07:54 +0200

    style new cron job page

--- admin/src/components/NewCronJob/index.tsx
+++ admin/src/components/NewCronJob/index.tsx
@@ -1,87 +1,81 @@
 import { Box } from "@strapi/design-system/Box";
 import { Button } from "@strapi/design-system/Button";
-import { Grid, GridItem } from "@strapi/design-system/Grid";
+import { Grid } from "@strapi/design-system/Grid";
+import { NumberInput } from "@strapi/design-system/NumberInput";
 import { Textarea } from "@strapi/design-system/Textarea";
 import { TextInput } from "@strapi/design-system/TextInput";
-import { Typography } from "@strapi/design-system/Typography";
-import React from "react";
+import React, { useState } from "react";
+
+const initialInput = {
+  name: "Test Job ",
+  schedule: "* * * * * *",
+  script: `console.log("Test Job ")`,
+  iterations: null,
+};
 
 export const NewCronJob: React.FunctionComponent = () => {
+  const [input, setInput] = useState(initialInput);
+
+  function handleInputChange(e: any) {
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value });
+  }
+
   return (
     <>
       <Box padding={8} background="neutral0">
-        <Grid gap={5}>
-          <GridItem col={3}>
-            <Box padding={2}>
-              <Typography variant="delta">Name</Typography>
-            </Box>
-          </GridItem>
-          <GridItem col={9}>
-            <Box padding={2}>
-              <TextInput
-                name="name"
-                required
-                placeholder="Cron Job name"
-                aria-label="Cron Job name input"
-              />
-            </Box>
-          </GridItem>
-          <GridItem col={3}>
-            <Box padding={2}>
-              <Typography variant="delta">Schedule</Typography>
-            </Box>
-          </GridItem>
-          <GridItem col={9}>
-            <Box padding={2}>
-              <TextInput
-                name="schedule"
-                required
-                placeholder="Cron Job schdule expression"
-                aria-label="Cron Job schedule expression input"
-              />
-            </Box>
-          </GridItem>
-          <GridItem col={3}>
-            <Box padding={2}>
-              <Typography variant="delta">Iterations</Typography>
-            </Box>
-          </GridItem>
-          <GridItem col={9}>
-            <Box padding={2}>
-              <Typography variant="omgea">
-                Number of iterations (optional)
-              </Typography>
-              <br />
-              <br />
-              <Typography variant="omgea">
-                default: "undefined" – unlimited iterations
-              </Typography>
-              <br />
-              <Typography variant="omgea">
-                {"<positive_integer>"} – finite number of iterations
-              </Typography>
-            </Box>
-          </GridItem>
-          <GridItem col={12}>
-            <Box padding={2}>
-              <Typography variant="delta">Script</Typography>
-            </Box>
-          </GridItem>
-          <GridItem col={12}>
-            <Box padding={2}>
-              <Textarea
-                name="script"
-                required
-                placeholder="Cron Job script"
-                aria-label="Cron Job script input"
-              />
-            </Box>
-          </GridItem>
-          <GridItem col={12}>
-            <Box padding={2}>
-              <Button size="L">Save</Button>
-            </Box>
-          </GridItem>
+        <Grid gap={5} gridCols={1}>
+          <Box padding={2}>
+            <TextInput
+              placeholder="Cron Job name"
+              required
+              label="Name"
+              name="name"
+              aria-label="Cron Job name input"
+              value={input.name}
+              onChange={handleInputChange}
+              // error={"Content is too long"}
+            />
+          </Box>
+          <Box padding={2}>
+            <TextInput
+              placeholder="Cron Job schdule expression"
+              required
+              label="Schedule"
+              name="schedule"
+              aria-label="Cron Job schedule expression input"
+              value={input.schedule}
+              onChange={handleInputChange}
+            />
+          </Box>
+          <Box padding={2}>
+            <NumberInput
+              placeholder="Number of iterations"
+              label="Iterations"
+              name="iterations"
+              hint="Default: unlimited"
+              aria-label="Number of iterations"
+              value={input.iterations}
+              onValueChange={(value) =>
+                handleInputChange({ target: { name: "iterations", value } })
+              }
+              // min value
+            />
+          </Box>
+          <Box padding={2}>
+            <Textarea
+              placeholder="Cron Job script"
+              required
+              label="Script"
+              name="script"
+              aria-label="Cron Job script input"
+              value={input.script}
+              onChange={handleInputChange}
+            />
+          </Box>
+          <Box padding={2}>
+            <Button size="L">Save</Button>
+          </Box>
         </Grid>
       </Box>
     </>
