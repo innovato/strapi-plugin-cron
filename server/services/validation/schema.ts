c6af150aed0a5919a52d450ca61246668ecb9c6e
mjnoach <a.siennkiewicz@gmail.com>
2022-10-17 13:37:23 +0200

    enable cron job start/end on current date

--- server/services/validation/schema.ts
+++ server/services/validation/schema.ts
@@ -1,8 +1,18 @@
 import { z } from "zod";
 
-const dateSchema = z.preprocess((arg) => {
+let startOfToday = new Date();
+startOfToday.setHours(0, 0, 0, 0);
+
+let endOfToday = new Date();
+endOfToday.setHours(23, 59, 59, 999);
+
+const startDateSchema = z.preprocess((arg) => {
   if (typeof arg == "string" || arg instanceof Date) return new Date(arg);
-}, z.date().min(new Date(), { message: "This date must be in the future" }).optional());
+}, z.date().min(startOfToday, { message: "This date can't be in the past" }).optional());
+
+const endDateSchema = z.preprocess((arg) => {
+  if (typeof arg == "string" || arg instanceof Date) return new Date(arg);
+}, z.date().min(endOfToday, { message: "This date can't be in the past" }).optional());
 
 export const CronJobSchema = z.object({
   name: z.string().min(1, { message: "This vaule is required" }),
@@ -12,6 +22,6 @@ export const CronJobSchema = z.object({
     .number()
     .min(-1, { message: "This value must be greater than or equal to -1" })
     .optional(),
-  startDate: dateSchema,
-  endDate: dateSchema,
+  startDate: startDateSchema,
+  endDate: endDateSchema,
 });
