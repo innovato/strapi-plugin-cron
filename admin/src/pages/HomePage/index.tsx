f33b95998dfe8763e1618cbbdece304da48ed77a
mjnoach <a.siennkiewicz@gmail.com>
2022-10-18 17:38:27 +0200

    add "create cron job" button to the top of the home page

--- admin/src/pages/HomePage/index.tsx
+++ admin/src/pages/HomePage/index.tsx
@@ -1,15 +1,20 @@
+import { Button } from "@strapi/design-system/Button";
 import { EmptyStateLayout } from "@strapi/design-system/EmptyStateLayout";
 import { BaseHeaderLayout, ContentLayout } from "@strapi/design-system/Layout";
 import { LoadingIndicatorPage } from "@strapi/helper-plugin";
 import EmptyDocuments from "@strapi/icons/EmptyDocuments";
+import Plus from "@strapi/icons/Plus";
 import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 import { CronJob } from "../../../../types";
 import { cron } from "../../api/cron";
 import { CronJobsList } from "../../components/CronJobsList";
+import { pluginBasePath } from "../../utils/plugin";
 
 export const HomePage: React.FunctionComponent = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [cronJobs, setCronJobs] = useState<CronJob[]>([]);
+  const history = useHistory();
 
   useEffect(() => {
     fetchCronJobs();
@@ -25,7 +30,18 @@ export const HomePage: React.FunctionComponent = () => {
 
   return (
     <>
-      <BaseHeaderLayout title="Cron Jobs" as="h2" />
+      <BaseHeaderLayout
+        title="Cron Jobs"
+        as="h2"
+        primaryAction={
+          <Button
+            startIcon={<Plus />}
+            onClick={() => history.push(`${pluginBasePath}/cron-jobs/create`)}
+          >
+            Add new Cron Job
+          </Button>
+        }
+      />
       <ContentLayout>
         {cronJobs.length === 0 ? (
           <EmptyStateLayout
