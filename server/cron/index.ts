134e4234e05863e9378c06f7514692500b296ace
mjnoach <a.siennkiewicz@gmail.com>
2022-11-30 09:50:21 +0100

    remove comments

--- server/cron/index.ts
+++ server/cron/index.ts
@@ -3,9 +3,6 @@ import { CronJob } from "../../types";
 import { createCronJobCallback } from "./cronJob";
 
 // TODO
-// - write documentation: { strapi, cronJob } parames are exposed to the script file, location of scipts
-// - execute script from a module file/ module/ callback/ hook/
-// TODO Extra
 // - Strapi DatePicker component bug: when date picker popover has been activated,
 //    and date has not been selected, and user navigates back with a click of a button
 //    a TypeError "source is null" is thrown
