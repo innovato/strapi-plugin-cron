ced726850bebdde712230333a92ac39de4a71a26
mjnoach <a.siennkiewicz@gmail.com>
2024-02-02 19:13:03 +0100

    update .gitignore

--- dist/server/bootstrap.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./cron/index");
-exports.default = ({ strapi }) => {
-    index_1.cron.init();
-};
--- dist/utils/plugin.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pluginName = void 0;
-const package_json_1 = __importDefault(require("../package.json"));
-exports.pluginName = package_json_1.default.strapi.name;
