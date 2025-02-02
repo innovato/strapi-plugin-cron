5f7f831bcd1b9ab4b21511761c8c39807debb648
mjnoach <a.siennkiewicz@gmail.com>
2025-02-02 21:03:25 +0100

    update home page

--- admin/src/pages/HomePage.tsx
+++ admin/src/pages/HomePage.tsx
@@ -1,14 +1,14 @@
-import { EmptyStateLayout, Main } from '@strapi/design-system';
-import { Loader } from '@strapi/icons';
+import { Button, EmptyStateLayout, Main } from '@strapi/design-system';
+import { Loader, Plus } from '@strapi/icons';
 import { EmptyDocuments } from '@strapi/icons/symbols';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { CronJob } from '../../../types';
-import { cron } from '../api/cron';
+import { pluginBasePath } from '../../../utils/plugin';
 import { CronJobsList } from '../components/CronJobsList';
+import { Header } from '../components/Header';
 
 const HomePage = () => {
-  // const { formatMessage } = useIntl();
   const [isLoading, setIsLoading] = useState(true);
   const [cronJobs, setCronJobs] = useState<CronJob[]>([]);
   const navigate = useNavigate();
@@ -18,8 +18,10 @@ const HomePage = () => {
   }, []);
 
   async function fetchCronJobs() {
-    const { data } = await cron.getAllCronJobs();
-    setCronJobs(data);
+    // TODO
+    // const { data } = await cron.getAllCronJobs();
+    // setCronJobs(data);
+    setCronJobs([]);
     setIsLoading(false);
   }
 
@@ -27,11 +29,9 @@ const HomePage = () => {
 
   return (
     <Main>
-      {/* <h1>Welcome to {formatMessage({ id: getTranslation('plugin.name') })}</h1> */}
-      Cron Jobs
-      {/* <BaseHeaderLayout
+      <Header
         title="Cron Jobs"
-        as="h2"
+        as="h1"
         primaryAction={
           <Button
             startIcon={<Plus />}
@@ -40,7 +40,7 @@ const HomePage = () => {
             Add new cron job
           </Button>
         }
-      /> */}
+      />
       <div>
         {cronJobs.length === 0 ? (
           <EmptyStateLayout
